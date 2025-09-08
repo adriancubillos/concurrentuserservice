3d4679fac33e073d1a22a41bc353943dad713de7
Adrian Cubillos <adriancubillos@gmail.com>
2025-09-08 12:11:13 -0400

    initial scafolding of Java Spring boot project

---
+++ src/main/java/com/amex/assesment/conc_users/ConcUsersApplication.java
@@ -0,0 +1,13 @@
+package com.amex.assesment.conc_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amex/assesment/conc_users/ConcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amex.assesment.conc_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
